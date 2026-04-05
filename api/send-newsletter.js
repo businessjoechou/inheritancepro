4ea23b1582eb1af5dc153ddf663c93fe1c386cb4
businessjoechou <businessjoechou@gmail.com>
2026-04-05 16:44:45 +0800

    fix: update reply_to to businessjoechou@gmail.com

--- api/send-newsletter.js
+++ api/send-newsletter.js
@@ -55,7 +55,7 @@ export default async function handler(req, res) {
           body: JSON.stringify({
             from: 'InheritancePro <notify@inheritancepro.app>',
             to: sub.email,
-            reply_to: 'happyjoechou@gmail.com',
+            reply_to: 'businessjoechou@gmail.com',
             subject: subject,
             html: html.replace('{{name}}', sub.name || '用戶')
           })
